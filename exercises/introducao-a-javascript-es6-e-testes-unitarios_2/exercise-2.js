d8738e79bebe77f6f6d07b5312a63c0ef9d3880a
Miguel Dantas <miiguelalb@gmail.com>
2020-12-02 16:19:16 -0300

    Part 2, 1/2 complete

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercise-2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercise-2.js
@@ -18,10 +18,47 @@ const lesson3 = {
   turno: 'noite',
 };
 
+// Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
 const addTurn = (object, key, value) => {
   object[key] = value
 }
 
-addTurn(lesson2, 'turno', 'manhã');
+// addTurn(lesson2, 'turno', 'manhã');
 
 // console.log(lesson2)
+
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const listKey = (object) => {
+ const obj = Object.keys(object);
+ return obj
+}
+
+// console.log(listKey(lesson2))
+
+// Crie uma função para mostrar o tamanho de um objeto.
+
+const objectTam = (object) => {
+  const obj = Object.keys(object).length;
+  return obj
+}
+
+// console.log(objectTam(lesson2))
+
+// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const objectValues = (object) => {
+  const obj = Object.values(object);
+  return obj
+}
+
+// console.log(objectValues(lesson2))
+
+// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+
+let allLessons = Object.assign({}, lesson1);
+allLessons = object.assign({}, lesson2);
+
+console.log(allLessons)
+
