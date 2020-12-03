331f3967f6c91b8cb938bd8b66d838c94408ac37
Miguel Dantas <miiguelalb@gmail.com>
2020-12-03 18:21:20 -0300

    ATT

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercise-2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercise-2.js
@@ -70,4 +70,5 @@ const totalNum = (object) => {
 
 // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
 
+// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
 
---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/scrp.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+const number = [2, 3, 4, 6, 8, 12, 24];
+
+const soma = number.reduce()
+
+const expectedValue = [];
+assert.strictEqual(soma, expectedValue);
