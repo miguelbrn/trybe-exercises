30786ef93b7e5da8afc34e74d663154b9a1c9c03
Miguel <miguel@pop-os.localdomain>
2021-02-18 13:56:19 -0300

    Todos os exercícios completos

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise5.js
@@ -0,0 +1,11 @@
+let degreeAngleA = 65;
+let degreeAngleB = 100;
+let degreeAngleC = 15;
+
+let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+
+if (sumOfAngles === 180) {
+  console.log('Ângulos válidos!');
+} else {
+  console.log('Ângulos válidos!');
+};
