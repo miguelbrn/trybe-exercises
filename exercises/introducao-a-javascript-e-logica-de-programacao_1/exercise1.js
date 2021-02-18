a1638f38e7eb5ada38ff991b8f8e50d4f7ff96fd
Miguel <miguel@pop-os.localdomain>
2021-02-18 13:47:42 -0300

    Primeiro Exercício completo

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise1.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 5;
+
+console.log(`Soma: ${a} + ${b} = ${a + b}`);
+console.log(`Subtração: ${a} - ${b} = ${a - b}`);
+console.log(`Multiplicação: ${a} * ${b} = ${a * b}`);
+console.log(`Divisão: ${a} / ${b} = ${a / b}`);
+console.log(`Módulo/Resto: ${a} % ${b} = ${a % b}`);
