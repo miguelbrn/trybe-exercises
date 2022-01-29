23806a69ed5b7e4c4be98a4bf05c20caeb5a0782
Miguel Dantas <miiguelalb@gmail.com>
2022-01-29 13:55:44 -0300

    Criado funcao de calcular IMC

---
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-scripts/imc.js
@@ -0,0 +1,21 @@
+// Crie um script para calcular o Índice de Massa Corporal(IMC) de uma pessoa.
+// A fórmula para calcular o IMC é peso / altura ^ 2 .
+// Obs: Lembre-se que a altura é em metros, caso deseje usar em centímetros a conversão para metros será necessária.
+// Comece criando um novo pacote node com npm init e respondendo às perguntas do npm .
+// Por enquanto, não se preocupe em pedir input da pessoa usuária. Utilize valores fixos para peso e altura .
+// Armazene o script no arquivo imc.js .
+// Agora, permita que o script seja executado através do comando npm run imc
+// O novo script criado deve conter o comando que chama o node para executar o arquivo imc.js .
+// Chegou a hora de tornar nosso script mais interativo! Vamos adicionar input de quem usa.
+// Você já utilizou o pacote readline-sync para esse fim. Que tal utilizar o mesmo pacote?
+// Substitua os valores fixos de peso e altura por dados informados pela pessoa ao responder as perguntas "Qual seu peso?" e "Qual sua altura?" no terminal.
+// Agora temos um problema: peso não é um número inteiro! Isso quer dizer que precisamos mudar um pouco a forma como solicitamos o input desse dado.
+// O pacote readline-sync possui uma função específica para tratar esses casos. Consulte a documentação do pacote e encontre o método adequado para realizar input de floats .
+// Encontrou a função? Show! Agora utilize-a para solicitar o input de peso .
+// Vamos sofisticar um pouco mais nosso script. Além de imprimir o IMC na tela, imprima também em qual categoria da tabela abaixo aquele IMC se enq
+
+const getImc = (peso, altura) => {
+  return peso / (Math.pow(altura, 2))
+}
+
+console.log(getImc(86, 1.82))
